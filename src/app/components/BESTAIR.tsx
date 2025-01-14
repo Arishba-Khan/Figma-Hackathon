b763c9ddcb87492fd6b7d086209d6b47e6ed168f
Arishba khan <cyberfre4k.com@gmail.com>
2025-01-14 12:51:51 +0500

    last changes in build error

--- src/app/components/BESTAIR.tsx
+++ src/app/components/BESTAIR.tsx
@@ -13,30 +13,29 @@ export default function BEST() {
       </div>
       <div className="flex flex-wrap justify-evenly items-center gap-4 sm:gap-5">
         {bestProducts.map((product: ProductDetail) => (
-         <Link href={"/detail"}>
-         <div
-            key={product.id}
-            className="flex flex-col text-sm sm:text-base leading-relaxed max-w-[150px] sm:max-w-[250px] lg:max-w-[300px]"
-          >
-            <Image
-              src={product.imageUrl}
-              alt={product.name}
-              width={300}
-              height={300}
-              className="w-full h-auto object-cover"
-            />
-            <div className="flex flex-wrap gap-4 sm:gap-10 items-start justify-between mt-3 bg-white">
-              <div className="flex flex-col text-xs sm:text-sm pr-1">
-                <div className="font-medium text-neutral-900">
-                  {product.name}
+          <Link key={product.id} href={"/detail"}> {/* Add key to Link component */}
+            <div
+              className="flex flex-col text-sm sm:text-base leading-relaxed max-w-[150px] sm:max-w-[250px] lg:max-w-[300px]"
+            >
+              <Image
+                src={product.imageUrl}
+                alt={product.name}
+                width={300}
+                height={300}
+                className="w-full h-auto object-cover"
+              />
+              <div className="flex flex-wrap gap-4 sm:gap-10 items-start justify-between mt-3 bg-white">
+                <div className="flex flex-col text-xs sm:text-sm pr-1">
+                  <div className="font-medium text-neutral-900">
+                    {product.name}
+                  </div>
+                  <div className="text-neutral-500">{product.description}</div>
                 </div>
-                <div className="text-neutral-500">{product.description}</div>
-              </div>
-              <div className="self-start font-medium text-right text-neutral-900">
-                {product.price}
+                <div className="self-start font-medium text-right text-neutral-900">
+                  {product.price}
+                </div>
               </div>
             </div>
-          </div>
           </Link>
         ))}
       </div>
